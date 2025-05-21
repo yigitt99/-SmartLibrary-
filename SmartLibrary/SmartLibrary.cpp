16eaecdff958e332d8538027a6a557602770ef6a
yigitt99 <yigitefee93@gmail.com>
2025-05-22 00:03:41 +0300

    Added user login system and admin control

--- SmartLibrary/SmartLibrary.cpp
+++ SmartLibrary/SmartLibrary.cpp
@@ -4,7 +4,11 @@
 #include <fstream>
 #include <sstream>
 #include "json.hpp"
+#include <thread>
 
+
+using namespace std::this_thread;
+using namespace std::chrono;
 using json = nlohmann::json;
 using namespace std;
 
@@ -24,7 +28,126 @@ struct Kullanici {
     string sifre;
     string rol;
 };
+
+
+vector<Kullanici> loadUsersFromJson(const string& filename) {
+    vector<Kullanici> users;
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "users.json dosyası açılamadı!" << endl;
+        return users;
+    }
+
+    json j;
+    file >> j;
+
+    for (const auto& item : j) {
+        Kullanici u;
+        u.kullanici_no = item["kullanici_no"];
+        u.ad = item["ad"];
+        u.sifre = item["sifre"];
+        u.rol = item["rol"];
+        users.push_back(u);
+    }
+
+    return users;
+}
+
+void clear();
+void intro();
+void login();
+bool isAdminLogin();
+
+void intro() {
+    cout << "\n\t##      ## ####### ##      ######  ###### ####  #### #######";
+    cout << "\n\t##      ## ##      ##      ##      ##  ## ##  ##  ## ##       ";
+    cout << "\n\t##  ##  ## #####   ##      ##      ##  ## ##  ##  ## #####      ";
+    cout << "\n\t##  ##  ## ##      ##      ##      ##  ## ##      ## ##           ";
+    cout << "\n\t####  #### ####### ####### ######  ###### ##      ## #######     ";
+    cout << "\n\n\t\t\t         !!!!!!!  !!!!!        ";
+    cout << "\n\t\t\t           !!     !   !      ";
+    cout << "\n\t\t\t           !!     !!!!!    ";
+
+    cout << "\n\n\n\t\t********  LIBRARY   PROJECT  *******";
+    cout << "\n\t\t====================================";
+    cout << "\n\n\t\t\t  MADE BY  :-";
+    cout << "\n\n\t\t\t  YIGIT EFE KILIC  ";
+    cout << " \n\n\n\t\t         Press any KEY to continue... ";
+    cin.get();
+    clear();
+
+}
+
+void clear() {
+	system("cls"); // Clear the console screen (Windows)
+}
+
+// Admin giriş kontrolü
+bool isAdminLogin() {
+    string adminUser = "admin";
+    string adminPassword = "admin123";
+    string username, password;
+
+    cout << "\n\tAdmin Girisi:\n";
+    cout << "\tKullanici adi: ";
+    cin >> username;
+    cout << "\tSifre: ";
+    cin >> password;
+
+    return (username == adminUser && password == adminPassword);
+}
+
+//Kullanıcı giriş fonksiyonu
+bool login(bool& isAdmin) {
+    vector<Kullanici> kullanicilar = loadUsersFromJson("users.json");
+
+    string girilenAd, girilenSifre;
+
+    do {
+        clear();
+        cout << "\n\t\t\t\t\t---------------------------------------------";
+        cout << "\n\t\t\t\t\t|                                           |";
+        cout << "\n\t\t\t\t\t|               W E L C O M E               |";
+        cout << "\n\t\t\t\t\t|                                           |";
+        cout << "\n\t\t\t\t\t---------------------------------------------";
+
+        cout << "\n\n\n\t\t\t\t\t\tKullanici adinizi girin: ";
+        cin >> girilenAd;
+
+        cout << "\n\n\t\t\t\t\t\tSifrenizi girin: ";
+        cin >> girilenSifre;
+
+        for (const auto& k : kullanicilar) {
+            if (k.ad == girilenAd && k.sifre == girilenSifre) {
+                clear();
+                cout << "\n\t\t\t\t\t---------------------------------------------";
+                cout << "\n\t\t\t\t\t|               P L E A S E                 |";
+                cout << "\n\t\t\t\t\t|                                           |";
+                cout << "\n\t\t\t\t\t|                   W A I T . . .           |";
+                cout << "\n\t\t\t\t\t---------------------------------------------";
+
+                cout << "\033[32m\n\t\t\t\t\t    Giris basarili. Hosgeldiniz, " << girilenAd << "!\033[0m" << endl;
+                cout << "\n\t\t\t\t\tLutfen Bekleyin Menuye Aktariliyorsunuz...";
+                sleep_for(seconds(1));
+
+                // Admin kontrolü
+                isAdmin = (k.rol == "admin");
+                return true; // Başarılı giriş
+            }
+        }
+
+        // Hatalı giriş
+        clear();
+        cout << "\n\t\t\t\t\t\033[31m    Hatali Giris! Lutfen tekrar deneyin.\033[0m" << endl;
+        sleep_for(seconds(1));
+
+    } while (true); // Doğru giriş yapılana kadar döngü devam eder
+}
 int main()
 {
+    intro();
 
+	bool isAdmin = false;
+
+    login(isAdmin);
 }
